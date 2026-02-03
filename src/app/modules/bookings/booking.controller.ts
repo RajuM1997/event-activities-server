4d3c48eb3546118edc85afc4492df6e7d439c802
RajuM1997 <mraju5797@gmail.com>
2026-02-03 13:40:05 +0600

    change payment account

--- src/app/modules/bookings/booking.controller.ts
+++ src/app/modules/bookings/booking.controller.ts
@@ -1,32 +1,35 @@
-import { Request, Response } from "express";
 import config from "../../../config";
+import { catchAsync } from "../../../utils/catchAsync";
+import sendResponse from "../../../utils/sendResponse";
 import { stripe } from "../../../helpers/stripe";
 import { BookingService } from "./booking.service";
+import { NextFunction, Request, Response } from "express";
 
-const handleStripeWebhookEvent = async (req: Request, res: Response) => {
-  const sig = req.headers["stripe-signature"] as string;
+const handleStripeWebhookEvent = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const sig = req.headers["stripe-signature"] as string;
+    const webhookSecret = config.stripe.stripe_web_hook_secret;
 
-  let event;
+    let event;
+    try {
+      event = stripe.webhooks.constructEvent(
+        req.body,
+        sig,
+        webhookSecret as string,
+      );
+    } catch (error) {
+      return res.status(400).send(`Webhook Error: ${error}`);
+    }
+    const result = await BookingService.handleStripeWebhookEvent(event);
 
-  try {
-    event = stripe.webhooks.constructEvent(
-      req.body, // RAW BUFFER
-      sig,
-      config.stripe.stripe_web_hook_secret as string,
-    );
-  } catch (err: any) {
-    console.error(
-      "❌ Stripe webhook signature verification failed:",
-      err.message,
-    );
-    return res.status(400).send(`Webhook Error: ${err.message}`);
-  }
-
-  await BookingService.handleStripeWebhookEvent(event);
-
-  // Stripe only needs 200 OK
-  return res.status(200).json({ received: true });
-};
+    sendResponse(res, {
+      success: true,
+      statusCode: 200,
+      message: "Payment send successfully",
+      data: result,
+    });
+  },
+);
 
 export const BookingController = {
   handleStripeWebhookEvent,
--- src/app/modules/bookings/booking.service.ts
+++ src/app/modules/bookings/booking.service.ts
@@ -3,28 +3,21 @@ import { PaymentStatus } from "@prisma/client";
 import prisma from "../../../utils/prisma";
 
 const handleStripeWebhookEvent = async (event: Stripe.Event) => {
-  console.log("Stripe event type:", event.type);
-
   switch (event.type) {
     case "checkout.session.completed": {
       const session = event.data.object as any;
       const bookingId = session.metadata?.bookingId;
 
-      if (!bookingId) return;
-
-      console.log(session);
-
-      await prisma.booking.updateMany({
+      await prisma.booking.update({
         where: {
           id: bookingId,
-          status: PaymentStatus.UNPAID,
         },
         data: {
           status:
             session.payment_status === "paid"
               ? PaymentStatus.PAID
               : PaymentStatus.UNPAID,
-          paymentGatewayData: JSON.parse(JSON.stringify(session)),
+          paymentGatewayData: session,
         },
       });
 
