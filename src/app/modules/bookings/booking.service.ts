66aff55bc0c5778b571bb5e67f3b5196b57605f0
RajuM1997 <mraju5797@gmail.com>
2026-02-03 13:23:54 +0600

    change payment route and service

--- src/app/modules/bookings/booking.service.ts
+++ src/app/modules/bookings/booking.service.ts
@@ -4,22 +4,27 @@ import prisma from "../../../utils/prisma";
 
 const handleStripeWebhookEvent = async (event: Stripe.Event) => {
   console.log("Stripe event type:", event.type);
+
   switch (event.type) {
     case "checkout.session.completed": {
       const session = event.data.object as any;
       const bookingId = session.metadata?.bookingId;
+
+      if (!bookingId) return;
+
       console.log(session);
 
-      await prisma.booking.update({
+      await prisma.booking.updateMany({
         where: {
           id: bookingId,
+          status: PaymentStatus.UNPAID,
         },
         data: {
           status:
             session.payment_status === "paid"
               ? PaymentStatus.PAID
               : PaymentStatus.UNPAID,
-          paymentGatewayData: session,
+          paymentGatewayData: JSON.parse(JSON.stringify(session)),
         },
       });
 
